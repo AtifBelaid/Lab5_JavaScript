048df5782ad5363551c23c3762a15be4985083eb
BelaidAtif <atif.belaid@gmail.com>
2020-05-08 11:52:05 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var fruits = ['mangue','raisin','figue','kiwi'];
+var fruits = document.getElementById("store").innerHTML=fruits;
+
+
+function myFonction() {
+    var result = false;
+    var fruit = document.getElementById("check").value;
+    for (i = 0 ; i < fruits.length; i++ ){
+    
+        if (fruit == fruits[i]) {
+            console.log("fruits "+fruits[i]);
+            console.log("fruit "+fruit);
+             result=true;
+        }
+    }
+
+    if (result==true){
+        alert("Ok!");
+
+    }else {
+        alert("Indisponible");
+    }
+
+    document.getElementById("store").innerHTML=fruits;
+    
+
+}
